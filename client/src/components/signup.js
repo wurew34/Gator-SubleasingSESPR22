67a2d343cd50a8b70d5dcece2822420af20cd6c0
Jackson Wu <jacksonwu@ufl.edu>
2022-01-30 17:57:36 -0500

    changes to signup and signin css

---
+++ client/src/components/signup.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { Grid, Paper, TextField, Button, Typography } from '@mui/material';
+import logo from'./Images/container logo.PNG';
+import './styles.css'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+
+class Signup extends Component {
+    render() {
+        const paperStyle = {padding : 20, height:'70vh', width:500, margin:"20px  auto"}
+      const theme = createTheme({
+        palette: {
+          background: {
+            default: "#FA8627",
+          } 
+        },
+      });
+        return(
+          <ThemeProvider theme={theme}>
+             <CssBaseline/>
+            <Grid>
+              <Paper elevation={10} style={paperStyle}>
+                <Grid align='center'>
+                  <img src={logo} alt="My logo"/>
+                  <h2>Sign Up</h2>
+                </Grid> 
+                <form>
+                <Grid container direction={"column"} spacing={3}>
+                 <Grid item>
+                <TextField label ='Enter First Name' placeholder='Enter First Name' fullWidth required/>
+                </Grid>
+                <Grid item>
+                <TextField label ='Enter Username' placeholder='Enter Username' fullWidth required/>
+                </Grid>
+                <Grid item>
+                <TextField label ='E-mail Address' placeholder='Enter E-mail Address' fullWidth required/>
+                </Grid>
+                <Grid item>
+                <TextField label ='Create Password' placeholder='Enter Password' type='password' fullWidth required/>
+                </Grid>
+                <Grid item>
+                <TextField label ='Confirm Password' placeholder='Enter Password' type='password' fullWidth required/>
+                </Grid>
+                <Grid item>
+                <Button variant="contained" type='submit' color='primary' fullWidth>Register</Button>
+                </Grid>
+                <Grid item>
+                <Typography align='center'>Already have an account?</Typography>
+                <a href="/login"><Typography> Sign in!</Typography></a>
+                </Grid> 
+                </Grid>
+                </form>
+              </Paper>
+              </Grid>
+              </ThemeProvider>
+        )
+    }
+}
+
+
+export default Signup;
