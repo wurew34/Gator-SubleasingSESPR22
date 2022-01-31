3f1f99378a38fc2af1c740bfa777fe9e25a9db2f
munisht06 <munishtanwar77@gmail.com>
2022-01-31 15:53:55 -0500

    particle animation added

---
+++ client/src/ParticleBackground.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Particles from "react-tsparticles";
+import particlesConfig from "./config/particle-config";
+
+export default function ParticleBackground() {
+  return <Particles params={particlesConfig} />;
+}
