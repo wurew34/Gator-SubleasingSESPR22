3dc39b97d92ccd5660fc26840d4bb0ffe02cdbff
Jackson Wu <jacksonwu@ufl.edu>
2022-01-31 19:51:31 -0500

    particle animations and resized login signup

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
 
 ReactDOM.render(
