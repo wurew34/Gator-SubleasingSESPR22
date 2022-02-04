049418cc099d8a2108a56a6aa7b328715ca51458
shreyas-kalikar <shreyaskalikar2000@gmail.com>
2022-02-03 21:35:43 -0500

    working on landing page

--- client/src/components/landing.js
+++ client/src/components/landing.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
-
-
+import imag from './Images/uf.jpg'
+import styles from './background.css';
 class Landing extends Component {
     render() {
         return(
             <div style={{width: '100%', margin: 'auto'}}>
                 <Grid className="landing-grid">
                     <Cell col={12}>
-                        <img
-                        src="https://static.techspot.com/images2/downloads/topdownload/2016/11/7dda385ed3724fea700d45a0349d9e77.png"
-                        alt="logo"
-                        className="logo-img"
-                        />
+                        
+                        
+
+                        <div className='image'>
+                            <image>
+                                 <img src={imag} alt="UF" style={{width: 1550, height: 600}}/>
+                                 
+
+                            </image>
+                            
+                            <h1 style={{color: "#FFFFFF", fontSize: 59, }}>
+                                Welcome to Gator Subleasing!
+                            </h1>
+                            
+                        </div>
+                        
 
                         <div className="banner-text">
-                            <h1>Welcome to Gator Subleasing!</h1>
+                            
                         </div>
 
                     </Cell>
