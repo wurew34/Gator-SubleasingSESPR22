073f11e2258d54b4a05394698aac20af69fd1cff
shreyas-kalikar <shreyaskalikar2000@gmail.com>
2022-03-04 21:57:13 -0500

    Flex added, text placed

--- client/src/components/listings.js
+++ client/src/components/listings.js
@@ -1,16 +1,26 @@
 import React from 'react'
-import { Paper } from '@mui/material';
+import { Paper, Typography  } from '@mui/material';
+import './listings.css';
 function Listings(){
     return(
-    <div>
+        
+        
+    <div class='container'>
         <Paper elevation={14}
         style={{
             padding:13,
             backgroundColor:"orange",
-            margin: "10px 10px 10px 10px"
+            margin: "10px 10px 10px 10px",
+            display:"flex",
+            alignItems: 'center',
         }}>
-            <img src="https://media.istockphoto.com/photos/large-house-with-steep-roof-and-side-entry-three-car-garage-picture-id1272163106?s=612x612" style={{width: 1200, height: 675,}}></img>
-            
+            <div class='image'>
+                <img src="https://media.istockphoto.com/photos/large-house-with-steep-roof-and-side-entry-three-car-garage-picture-id1272163106?s=612x612" style={{width: 1200, height: 675,}}></img>
+            </div>
+            <div class='text-col'>
+                
+                <h1>Sample text</h1>
+            </div>
         </Paper>
     </div>
     )
