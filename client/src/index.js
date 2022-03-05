33902244c0aade72f1d0e3568f0793910d10b145
Jackson Wu <jacksonwu@ufl.edu>
2022-03-04 23:08:31 -0500

    added e2e and unit testing using jest and cypress

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import 'react-mdl/extra/material.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import "react-mdl/extra/material.js";
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
-    </BrowserRouter>
-  , document.getElementById('root')
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
