084f2e78cfb4710775ea36d5508bc8d684c057fa
Jackson Wu <jacksonwu@ufl.edu>
2022-03-28 16:04:53 -0400

    changed search bar styling

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -235,9 +235,7 @@ const Dashboard = () => {
           </Toolbar>
         </AppBar>
       </Box>
-      <Box
-        sx={{ marginTop: 12, paddingLeft: 4, paddingRight: 5, marginLeft: 10 }}
-      >
+      <Grid sx={{ marginTop: 12, marginLeft: 10 }} container>
         <Button
           color="primary"
           variant="contained"
@@ -247,10 +245,8 @@ const Dashboard = () => {
         >
           Create Listing
         </Button>
-        <Search
-          setQuery={(search) => setSearchQuery(search)}
-        />
-        <FormControl sx={{ marginLeft: 170 }} className={classes.root}>
+        <Search setQuery={(search) => setSearchQuery(search)} />
+        <FormControl sx={{ marginLeft: 50 }} className={classes.root}>
           <Select
             placeholder="Sort by..."
             value={sort}
@@ -258,7 +254,7 @@ const Dashboard = () => {
             options={data}
           />
         </FormControl>
-      </Box>
+      </Grid>
       <Paper
         elevation={5}
         sx={{
