c5eb4e09f2e95fd8c9884ae7c16cff6d74c0db24
Jackson Wu <jacksonwu@ufl.edu>
2022-04-20 14:39:11 -0400

    server deploy

--- server/configs/db_setup.go
+++ server/configs/db_setup.go
@@ -13,7 +13,7 @@ import (
 )
 
 func ConnectDB() *mongo.Client {
-	client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("DB_URI")))
+	client, err := mongo.NewClient(options.Client().ApplyURI(GetEnv("DB_URI")))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -41,12 +41,12 @@ func GetCollection(client *mongo.Client, collectionName string) *mongo.Collectio
 	return client.Database("gatorSubleasing").Collection(collectionName)
 }
 
-// func GetEnv(key string) string {
-// 	err := godotenv.Load()
-// 	if err != nil {
-// 		log.Fatal(err)
-// 		log.Fatal("Error loading .env file")
-// 		panic(err)
-// 	}
-// 	return os.Getenv(key)
-// }
+func GetEnv(key string) string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+		log.Fatal("Error loading .env file")
+		panic(err)
+	}
+	return os.Getenv(key)
+}
